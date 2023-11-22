53f089552c6861b1507a9241825eca245d33211c
Alain Nambi <alainnambi@gmail.com>
2023-11-22 10:33:28 +0300

    feat: add date formatting and unique filename generation
    - Implement 'formatDate' function to trim and replace newline characters in date strings.
    - Introduce 'uniqueFileName' function for generating unique PDF filenames based on the current date and time.
    - Utilize ISO format for obtaining the date and time.
    - Enhance readability by breaking down the functionality into two separate functions.
    - Provide descriptive comments for each function to explain their purpose and usage.

---
+++ utilities/format.js
@@ -0,0 +1,11 @@
+export const formatDate = (date) => {
+  return String(date).trim("\n").replaceAll("\n", "-");
+};
+
+export const uniqueFileName = () => {
+  const datetime = new Date().toISOString().split("T");
+  const date = datetime[0].replaceAll("-", "_")
+  const time = datetime[1].split(".")[0].replaceAll(":", "_")
+  const milliseconds = datetime[1].split(".")[1].replace("Z", "")
+  return `scarpe_on_${date}_${time}_${milliseconds}.pdf`
+};
