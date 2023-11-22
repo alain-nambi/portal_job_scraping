7a0cde4fdbcf12a70bffa5358ecd5a1cbad771db
Alain Nambi <alainnambi@gmail.com>
2023-11-22 10:32:26 +0300

    feat: add web scraping and data export functions
    - Create a module for web scraping job data and exporting it.
    - Introduce a function to find text by HTML tag.
    - Implement a web scraping function using Cheerio and Axios to extract job data from a specified URL.
    - Utilize an ora spinner to provide visual feedback during the scraping process.
    - Handle successful data retrieval and export the results to a PDF file using the exportToPDF function.
    - Add error handling to manage unexpected issues during the scraping operation.
    - Set the number of pages to scrape (pageList) for better control over the process.

---
+++ utilities/scrape.js
@@ -0,0 +1,85 @@
+import cheerio from "cheerio";
+import axios from "axios";
+import chalk from "chalk";
+import ora from "ora";
+import {
+  contenuAnnonceSelector,
+  itemAnnonceSelector,
+  itemDetailSelector,
+} from "../constants/index.js";
+import { extractJobData } from "./extract.js";
+import { exportToPDF } from "./export.js";
+import { uniqueFileName } from "./format.js";
+
+const webDevUrl =
+  "https://www.portaljob-madagascar.com/emploi/liste/secteur/informatique-web/page/";
+
+const findTextByHTMLTag = ($annonce, HTMLTag) => {
+  const textFound = $annonce
+    .find(contenuAnnonceSelector)
+    .find(HTMLTag)
+    .text()
+    .trim();
+  if (textFound) {
+    return textFound;
+  } else {
+    console.log("Aucune texte n'a été récupérée");
+  }
+};
+
+const scrapeJobData = async () => {
+  const loadingSpinnerScrapping = ora({
+    text: chalk.white(
+      "Récupération des données sur les offres d'emploi en ligne..."
+    ),
+    spinner: "dots",
+    color: "white",
+  });
+
+  loadingSpinnerScrapping.start();
+
+  try {
+    const jobLists = [];
+    const pageList = 10;
+
+    for (let i = 0; i <= pageList; i++) {
+      const result = await axios.get(webDevUrl + i);
+      if (result.data) {
+        const $ = cheerio.load(result.data);
+        const itemAnnounce = $(itemAnnonceSelector);
+
+        if (itemAnnounce) {
+          itemAnnounce.each(async function (_index, annonce) {
+            const jobData = await extractJobData($(annonce));
+
+            if (jobData.title && jobData.company && jobData.contractType) {
+              jobLists.push(jobData);
+            }
+          });
+        }
+      }
+    }
+
+    if (jobLists) {
+      loadingSpinnerScrapping.succeed(
+        chalk.white("Récupération des données terminées !")
+      );
+
+      exportToPDF(jobLists, uniqueFileName());
+    } else {
+      loadingSpinnerScrapping.fail(
+        chalk.redBright(
+          "Aucune donnée récupérée. Veuillez vérifier l'URL ou réessayer plus tard"
+        )
+      );
+    }
+  } catch (error) {
+    chalk.redBright(
+      "\n Une erreur s'est produite lors de la récupération des données"
+    );
+    console.error(error.message);
+    loadingSpinnerScrapping.stop();
+  }
+};
+
+export { findTextByHTMLTag, scrapeJobData };
